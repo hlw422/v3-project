66f0717f3855103d802bbef2ae062a8d042dafda
hlw422 <hlw422@126.com>
2024-03-18 20:56:50 +0800

    增加子组件给父组件传递消息

--- src/FatherCom.vue
+++ src/FatherCom.vue
@@ -2,15 +2,26 @@
 import { ref } from "vue";
 import sonCom from "./sonCom.vue";
 
+//获取子组件数据
+const getMessage=(msg)=>{
+  console.log(msg)
+}
 const count = ref(100);
 
 setTimeout(() => {
     count.value=200
 }, 3000);
+
+
+
 </script>
 <template>
   <div class="father">
     <h2>父组件</h2>
     <sonCom :count="count" message="father message" />
+  </div>
+  <sonCom @get-message="getMessage"/>
+  <div>
+
   </div>
 </template>
--- src/sonCom.vue
+++ src/sonCom.vue
@@ -5,6 +5,12 @@ const props=defineProps({
     count:Number
 })
 console.log(props)
+
+
+const emit=defineEmits(['get-message'])
+const sendMsg=()=>{
+    emit('get-message','this is son msg')
+}
 </script>
 <template>
     <div class="son">
@@ -12,5 +18,8 @@ console.log(props)
         <div>
             父组件传入的数据-{{message}}{{ count }}
         </div>
+    </div>
+    <div>
+        <button @click="sendMsg">给父组件传消息1</button>
     </div>
 </template>
