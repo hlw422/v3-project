6696fb6501ef8cf46af0ad6e0a1f7a07b78350e0
hlw422 <hlw422@126.com>
2024-03-22 21:40:24 +0800

    pinia getter,异步操作

--- src/piniaEx/CountTest.vue
+++ src/piniaEx/CountTest.vue
@@ -1,11 +1,31 @@
 <script setup>
 import {useCounterStore} from  '@/piniaEx/counter'
+import { onMounted } from 'vue';
+import { storeToRefs } from 'pinia';
 
 const counterStore=useCounterStore()
 console.log(counterStore)
+
+//直接解构赋值 不是响应式
+//const {count,doubleCount}=counterStore
+//方法包裹（保持响应式更新）
+const {count,doubleCount}=storeToRefs(counterStore)
+//方法直接从counterStore中解构赋值
+const {increment}=counterStore
+onMounted(()=>{
+    counterStore.getList()
+})
 </script>
 <template>
     <div>
-        <button @click="counterStore.increment">{{ counterStore.count }}</button>
+        <button @click="counterStore.increment">{{ count }}</button>
+    </div>
+    <div>
+        <h1>{{ counterStore.doubleCount }}</h1>
+    </div>
+    <div>
+        <ul>
+            <li v-for="item in counterStore.list" :key="item.id">{{ item.name }}</li>
+        </ul>
     </div>
 </template>
--- src/piniaEx/counter.js
+++ src/piniaEx/counter.js
@@ -1,6 +1,10 @@
 
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { computed, ref } from "vue";
+import axios from "axios";
+
+
+const API_URL="https://geek.itheima.net/v1_0/channels"
 
 export const useCounterStore = defineStore("counter", () => {
   const count = ref(0);
@@ -8,5 +12,124 @@ export const useCounterStore = defineStore("counter", () => {
     count.value++;
   }
 
-  return { count, increment };
+/*
+
+{
+    "data": {
+        "channels": [
+            {
+                "id": 0,
+                "name": "推荐"
+            },
+            {
+                "id": 1,
+                "name": "html"
+            },
+            {
+                "id": 2,
+                "name": "开发者资讯"
+            },
+            {
+                "id": 4,
+                "name": "c++"
+            },
+            {
+                "id": 6,
+                "name": "css"
+            },
+            {
+                "id": 7,
+                "name": "数据库"
+            },
+            {
+                "id": 8,
+                "name": "区块链"
+            },
+            {
+                "id": 9,
+                "name": "go"
+            },
+            {
+                "id": 10,
+                "name": "产品"
+            },
+            {
+                "id": 11,
+                "name": "后端"
+            },
+            {
+                "id": 12,
+                "name": "linux"
+            },
+            {
+                "id": 13,
+                "name": "人工智能"
+            },
+            {
+                "id": 14,
+                "name": "php"
+            },
+            {
+                "id": 15,
+                "name": "javascript"
+            },
+            {
+                "id": 16,
+                "name": "架构"
+            },
+            {
+                "id": 17,
+                "name": "前端"
+            },
+            {
+                "id": 18,
+                "name": "python"
+            },
+            {
+                "id": 19,
+                "name": "java"
+            },
+            {
+                "id": 20,
+                "name": "算法"
+            },
+            {
+                "id": 21,
+                "name": "面试"
+            },
+            {
+                "id": 22,
+                "name": "科技动态"
+            },
+            {
+                "id": 23,
+                "name": "js"
+            },
+            {
+                "id": 24,
+                "name": "设计"
+            },
+            {
+                "id": 25,
+                "name": "数码产品"
+            },
+            {
+                "id": 26,
+                "name": "软件测试"
+            }
+        ]
+    },
+    "message": "OK"
+}*/
+  //getter定义
+  const doubleCount=computed(()=>count.value*2)
+  //异步action
+  const list=ref([])
+  const getList=async()=>{
+     const res=await axios.get(API_URL)
+     list.value=res.data.data.channels
+
+  }
+
+  return { count, increment,doubleCount,list,getList };
 });
