ed67b5519a966500888f461e5a3d6d09ae5b957e
hlw422 <hlw422@126.com>
2024-03-22 20:12:52 +0800

    pinia 安装和基础使用

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 //import App from './App.vue'
 //import father from './FatherCom.vue'
 //import father from './refCom.vue'
-import father from './dataCrossCom/room-page.vue'
-createApp(father).mount('#app')
+//import father from './dataCrossCom/room-page.vue'
+import father from './piniaEx/CountTest.vue'
+const pinia = createPinia()
+createApp(father).use(pinia).mount('#app')
---
+++ src/piniaEx/CountTest.vue
@@ -0,0 +1,11 @@
+<script setup>
+import {useCounterStore} from  '@/piniaEx/counter'
+
+const counterStore=useCounterStore()
+console.log(counterStore)
+</script>
+<template>
+    <div>
+        <button @click="counterStore.increment">{{ counterStore.count }}</button>
+    </div>
+</template>
